8945709f44278083ecb1cacb0592c4ade8a93cbb
Jonathan Hsu <jonhsu.design@gmail.com>
2020-06-04 10:46:29 -0700

    set up env file

--- api/server.js
+++ api/server.js
@@ -10,7 +10,11 @@ server.use(helmet());
 server.use('/api', apiRouter);
 
 server.get('/', (req,res) => {
-  res.status(200).json({ environment: process.env.NODE_ENV, port: process.env.PORT });
+  res.status(200).json({
+    environment: process.env.NODE_ENV,
+    port: process.env.PORT,
+    greeting: process.env.GREETING
+  });
 });
 
 module.exports = server;
